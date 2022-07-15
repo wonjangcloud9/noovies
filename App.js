6556401b5945c610143e311592a6cf914d902898
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-07-16 01:01:01 +0900

    hook useAssets & useFonts

--- App.js
+++ App.js
@@ -4,40 +4,15 @@ import { Text, Image } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
 
 SplashScreen.preventAutoHideAsync();
 
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-const loadImages = (images) =>
-  images.map((image) => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.loadAsync(image);
-    }
-  });
-
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const startLoading = async () => {
-    const fonts = await loadFonts([Ionicons.font]);
-    const images = loadImages([
-      require("./me.png"),
-      "https://i.imgur.com/tGbaZCY.jpg",
-    ]);
-    await Promise.all([...fonts, ...images]);
-  };
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={() => {
-          setReady(true);
-        }}
-        onError={console.error}
-      />
-    );
+  const [asset] = useAssets([require("./me.png")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+  if (!asset || !loaded) {
+    return <AppLoading />;
   }
   return <Text>We are done loading</Text>;
 }
