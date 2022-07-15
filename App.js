ff31da0d6543694f15993aad592d45f25cee145e
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-07-16 00:33:27 +0900

    preload font,asset

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
-
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [ready, setReady] = useState(false);
-  const startLoading = async () => {};
+  const startLoading = async () => {
+    await Font.loadAsync(Ionicons.font);
+    await Asset.loadAsync([require("./me.png")]);
+    await Image.prefetch("https://i.imgur.com/tGbaZCY.jpg");
+  };
   if (!ready) {
     return (
       <AppLoading
