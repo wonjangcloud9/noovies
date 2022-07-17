a304d8136c890e95206442ff93d4b67fea4bbb2d
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-07-17 23:57:48 +0900

    darkmode

---
+++ colors.js
@@ -0,0 +1,2 @@
+export const Black = `#000`;
+export const White = `#fff`;
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,31 +3,35 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
+import { useColorScheme } from "react-native";
+import { Black, White } from "../colors";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-  <Tab.Navigator
-    initialRouteName="Movies"
-    screenOptions={{
-      tabBarLabelStyle: {
-        backgroundColor: "red",
-      },
-    }}
-  >
-    <Tab.Screen name="Movies" component={Movies} />
-    <Tab.Screen
-      name="Tv"
-      component={Tv}
-      options={{
-        tabBarLabelStyle: {
-          backgroundColor: "purple",
+const Tabs = () => {
+  const isDark = useColorScheme() === "dark";
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: isDark ? Black : White,
         },
-        tabBarBadge: 5,
+        tabBarActiveTintColor: isDark ? White : Black,
+        tabBarInActiveTintColor: isDark ? White : Black,
+        headerStyle: {
+          backgroundColor: isDark ? Black : White,
+          borderBottomColor: isDark ? Black : White,
+        },
+        headerTitleStyle: {
+          color: isDark ? White : Black,
+        },
       }}
-    />
-    <Tab.Screen name="Search" component={Search} />
-  </Tab.Navigator>
-);
+    >
+      <Tab.Screen name="Movies" component={Movies} />
+      <Tab.Screen name="Tv" component={Tv} />
+      <Tab.Screen name="Search" component={Search} />
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
