8cb4829c8ec01368a2886f652d8aacf56ee4f5fd
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-07-18 23:44:24 +0900

    theme

--- App.js
+++ App.js
@@ -5,17 +5,23 @@ import * as SplashScreen from "expo-splash-screen";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset, useAssets } from "expo-asset";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  DarkTheme,
+  DefaultTheme,
+} from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
+import { useColorScheme } from "react-native";
 
 export default function App() {
+  const isDark = useColorScheme() === "dark";
   const [asset] = useAssets([require("./me.png")]);
   const [loaded] = Font.useFonts(Ionicons.font);
   if (!asset || !loaded) {
     return <AppLoading />;
   }
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
       <Tabs />
     </NavigationContainer>
   );
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,30 +3,13 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
-import { useColorScheme } from "react-native";
 import { Black, White } from "../colors";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-  const isDark = useColorScheme() === "dark";
   return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarStyle: {
-          backgroundColor: isDark ? Black : White,
-        },
-        tabBarActiveTintColor: isDark ? White : Black,
-        tabBarInActiveTintColor: isDark ? White : Black,
-        headerStyle: {
-          backgroundColor: isDark ? Black : White,
-          borderBottomColor: isDark ? Black : White,
-        },
-        headerTitleStyle: {
-          color: isDark ? White : Black,
-        },
-      }}
-    >
+    <Tab.Navigator>
       <Tab.Screen name="Movies" component={Movies} />
       <Tab.Screen name="Tv" component={Tv} />
       <Tab.Screen name="Search" component={Search} />
