0e0abe3f98c66dd911f787319437c06368b77c90
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-07-21 16:36:08 +0900

    theme

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
-import { Text, Image } from "react-native";
+import { Text, Image, useColorScheme } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
@@ -9,16 +9,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
 import Stack from "./navigation/Stack";
 import Root from "./navigation/Root";
+import { ThemeProvider } from "styled-components/native";
+import { darkTheme, lightTheme } from "./styled";
 
 export default function App() {
+  const isDark = useColorScheme() === "dark";
   const [asset] = useAssets([require("./me.png")]);
   const [loaded] = Font.useFonts(Ionicons.font);
   if (!asset || !loaded) {
     return <AppLoading />;
   }
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? lightTheme : darkTheme}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
