b77e28f56934d921354b5021348c2e60aff9d47a
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-07-26 13:43:06 +0900

    fix: vote

--- components/Votes.tsx
+++ components/Votes.tsx
@@ -5,10 +5,15 @@ interface VotesProps {
   votes: number;
 }
 
-const Text = styled.Text``;
+const Text = styled.Text`
+  color: rgba(255, 255, 255, 0.8);
+  font-size: 10px;
+`;
 
 const Votes: React.FC<VotesProps> = ({ votes }) => {
-  return <Text>{votes > 0 ? `⭐️ ${votes}/10` : "Coming Soon"}</Text>;
+  return (
+    <Text>{votes > 0 ? `⭐️ ${votes.toFixed(1)}/10` : "Coming Soon"}</Text>
+  );
 };
 
 export default Votes;
