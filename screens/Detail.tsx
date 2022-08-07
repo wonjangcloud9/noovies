fd762aa46a67dc2b8b8397f41587dcb76ccbb565
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-08-07 23:51:06 +0900

    detail typescript

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,8 +1,19 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
 import { Text, View } from "react-native";
 import styled from "styled-components/native";
 
-const Detail = ({
+const Container = styled.ScrollView`
+  background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+type RootStackParamList = {
+  Detail: { originalTitle: string };
+};
+
+type DetailScreenProps = NativeStackScreenProps<RootStackParamList, "Detail">;
+
+const Detail: React.FC<DetailScreenProps> = ({
   navigation: { setOptions },
   route: {
     params: { originalTitle },
