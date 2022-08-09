5845acdd62058eb7a7d1834086344957dfd66b23
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-08-09 23:14:16 +0900

    detail ui

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,14 +1,42 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
-import { Text, View } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import styled from "styled-components/native";
 import { Movie, TV } from "../api";
 import Poster from "../components/Poster";
+import { makeImgPath } from "../util";
+import { LinearGradient } from "expo-linear-gradient";
+import { Black } from "../colors";
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
 `;
 
+const Header = styled.View`
+  height: ${SCREEN_HEIGHT * 0.4}px;
+  justify-content: flex-end;
+  padding: 0 20px;
+`;
+
+const Background = styled.Image``;
+const Column = styled.View`
+  flex-direction: row;
+  width: 80%;
+`;
+const Title = styled.Text`
+  color: white;
+  font-size: 36px;
+  align-self: flex-end;
+  margin-left: 15px;
+  font-weight: 500;
+`;
+const Overview = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  margin-top: 20px;
+  padding: 0 20px;
+`;
 type RootStackParamList = {
   Detail: Movie | TV;
 };
@@ -21,14 +49,30 @@ const Detail: React.FC<DetailScreenProps> = ({
 }) => {
   useEffect(() => {
     setOptions({
-      title: "original_title" in params ? params.original_title : params.name,
+      title: "original_title" in params ? "Movie" : "TV Show",
     });
   });
 
   return (
-    <View>
-      <Poster path={params.poster_path || ""} />
-    </View>
+    <Container>
+      <Header>
+        <Background
+          style={StyleSheet.absoluteFill}
+          source={{ uri: makeImgPath(params.backdrop_path || "") }}
+        />
+        <LinearGradient
+          colors={["transparent", Black]}
+          style={StyleSheet.absoluteFill}
+        />
+        <Column>
+          <Poster path={params.poster_path || ""} />
+          <Title>
+            {"original_title" in params ? params.original_title : params.name}
+          </Title>
+        </Column>
+      </Header>
+      <Overview>{params.overview}</Overview>
+    </Container>
   );
 };
 
